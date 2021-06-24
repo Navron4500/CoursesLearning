764706f4a1afc4448526d830420aa344243fc2de
Naveen-Majnu <nagrawal.na1041@gmail.com>
2021-06-24 19:28:24 +0530

    Completed Password Creator And Started Calculator GUI

--- Coding Den/Practice.py
+++ Coding Den/Practice.py
@@ -1,15 +1,25 @@
-n = [1,2,3,4,5,6,7,8,9,-1]
+import tkinter as tk
+Calculator = tk.Tk()
+Calculator.title('Calculator')
 
-def arrayP(n, op=0):
-    while n:
-        n.remove(max(n))
-        op += max(n)
-        n.remove(max(n))
-        n.remove(max(n))
-        op += max(n)
-        n.remove(max(n))
-    return op
-
-print(arrayP(n))
+# Creatingn Input interface
+tk.Label(Calculator,text="Number 1").grid(row=0)
+tk.Label(Calculator,text="Number 2").grid(row=1)
+ip1 = tk.Entry(Calculator)
+ip2 = tk.Entry(Calculator)
+ip1.grid(row=0,column=1)
+ip2.grid(row=1,column=1)
 
 
+
+def showOutput(ip1,ip2):
+    res = int(ip1) + int(ip2)
+    w = tk.Label(Calculator, text=str(res))
+
+# Creating Add Button
+add_button = tk.Button(Calculator, text="Add", width=25, command=showOutput(ip1,ip2)).grid(row=3)
+add_button.pack()
+
+
+
+Calculator.mainloop()
