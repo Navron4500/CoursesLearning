4ce393b00931d44781e51f93068a0903c64877da
Naveen-Majnu <nagrawal.na1041@gmail.com>
2021-06-26 13:27:12 +0530

    Nothing much just Cali planing

--- Coding Den/Practice.py
+++ Coding Den/Practice.py
@@ -1,24 +1,11 @@
 import tkinter as tk
+from functools import partial
+
+
 Calculator = tk.Tk()
 Calculator.title('Calculator')
 
-# Creatingn Input interface
-tk.Label(Calculator,text="Number 1").grid(row=0)
-tk.Label(Calculator,text="Number 2").grid(row=1)
-ip1 = tk.Entry(Calculator)
-ip2 = tk.Entry(Calculator)
-ip1.grid(row=0,column=1)
-ip2.grid(row=1,column=1)
-
-
-
-def showOutput(ip1,ip2):
-    res = int(ip1) + int(ip2)
-    w = tk.Label(Calculator, text=str(res))
-
-# Creating Add Button
-add_button = tk.Button(Calculator, text="Add", width=25, command=showOutput(ip1,ip2)).grid(row=3)
-add_button.pack()
+ip = tk.Entry(Calculator)
 
 
 
