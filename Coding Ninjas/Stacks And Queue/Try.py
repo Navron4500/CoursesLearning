15786c9d985e245e1426228b68740caa575e7ea8
Naveen-Majnu <nagrawal.na1041@gmail.com>
2021-07-03 11:02:56 +0530

    Completed Queue Lectures

--- Coding Ninjas/Stacks And Queue/Try.py
+++ Coding Ninjas/Stacks And Queue/Try.py
@@ -1,24 +1,57 @@
-def stockSpan(price, n) :
-    stack = []
-    op = []
-    for index,value in enumerate(price):
-        if (index == 0 or price[stack[-1]]>value):
-            op.append(1)
-            stack.append(index)
+import queue
+class Stack :
+    
+	#Define data members and __init__()
+    def __init__(self):
+        self.__q1 = queue.Queue()
+        self.__q2 = queue.Queue()
+        self.__top = 0
+        
+    def getSize(self) :
+        return self.__q1.qsize()
+    
+    def isEmpty(self) :
+        return self.getSize() == 0
+    
+    def push(self, data) :
+        self.__q1.put(data)
+        self.__top = data
+        
+    def pop(self) :
+		#Implement the pop() function
+        if self.isEmpty():
+            return -1
+        
+        while self.__q1.qsize() > 1:
+            self.__q2.put(self.__q1.get())
+            
+        data = self.__q1.get()
+        
+        
+        while self.__q2.qsize() > 1:
+            self.__q1.put(self.__q2.get())
 
+        if not self.__q2.empty() :
+            self.__top = self.__q2.get()
+            self.__q1.put(self.__top)
         else:
-            while stack and price[stack[-1]] < value:
-                stack.pop()
-            
-            if stack:
-                op.append(index-stack[-1])
-            else:
-                op.append(index+1)
-            
-            stack.append(index)
-    return op
+            self.__top = -1
+        
+        return data 
+    
+    def top(self) :
+		#Implement the top() function
+        if self.isEmpty():
+            return -1
+        return self.__top
 
-arr = [100, 80, 60, 70, 60, 75, 85]
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+while s.top() != -1:
+    print(s.pop())
+print(s.isEmpty())
 
-# arr = list(map(int,input().split()))
-print(stockSpan(arr,len(arr)))
+
+        
