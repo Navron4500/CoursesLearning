e3a511fb33918b9f4bad742db8a7ea1839543f8f
Naveen-Majnu <nagrawal.na1041@gmail.com>
2021-07-24 15:32:35 +0530

    Partially completed BST 2

--- Coding Ninjas/BST_1/try.py
+++ Coding Ninjas/BST_1/try.py
@@ -1,94 +1,72 @@
-import queue
 class BinaryTreeNode:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
         
-def printLevelWise(root):
-    # Your code goes here
-    if not root:
-        return 
+class BST:
     
-    q = queue.Queue()
-    q.put(root)
-    while not q.empty():
-        current_node = q.get()
-        print(current_node.data,end=":")
-        if current_node.left:
-            print("L:" + str(current_node.left.data),end=", ")
-            q.put(current_node.left)
-        else:
-            print("L: ",end=", ")
-            
-            
-        if current_node.right:
-            print("R:" + str(current_node.right.data),end=" ")
-            q.put(current_node.right)
-        else:
-            print("R: ",end=" ")
+    def __init__(self) -> None:
+        self.root = None
+        self.numNodes = 0
         
-        print("")
+    def isPresentHelper(self,root,data):
+        if not root:
+            None
 
-
-def levelWiseTreeInput():
-    q = queue.Queue()
-    rootData = input("Enter Root \n")
-    if not rootData:
-        return None
-    
-    root = BinaryTreeNode(int(rootData))
-    q.put(root)
-    while not q.empty():
-        current_Node = q.get()
-
-        print("Enter Left Child of ", current_Node.data)
-        leftChidData = input()
-        if leftChidData:
-            leftChild = BinaryTreeNode(int(leftChidData))
-            current_Node.left = leftChild
-            q.put(leftChild)
-
+        if root.data == data:
+            return True
         
-        print("Enter Right Child of ", current_Node.data)
-        rightChildData = input()
-        if rightChildData:
-            rightChild = BinaryTreeNode(int(rightChildData))
-            current_Node.right = rightChild
-            q.put(rightChild)
+        if root.data > data: 
+            self.isPresentHelper(data.left,data)
+        else:
+            return self.isPresentHelper(data.right,data)
+        
+    def isPresent(self,data):
+        return self.isPresentHelper(self.root,data)
+        
+    def insertHelper(self,root,data):
+        if not root:
+            node = BinaryTreeNode(data)
+            return node
+        
+        if root.data > data:
+            root.left = self.insertHelper(root.left,data)
+        else:
+            root.right = self.insertHelper(root.right,data)
+        
+        return root
 
-    return root
+    def insert(self,data):
+        self.insertHelper(self.root,data)
 
-def maxTree(root):
-    if not root:
-        return -100000
-    leftMax, rightMax = maxTree(root.left), maxTree(root.right)
-    return max(leftMax,rightMax,root.data)
-
-def minTree(root):
-    if not root:
-        return 100000
-    leftMin, rightMin = minTree(root.left), minTree(root.right)
-    return min(root.data, leftMin,rightMin)
-
-def isBST2(root,min=-10000,max=10000):
-    if not root:
+    def deleteData(self,data):
         return True
 
-    if (root.data < min) or (root.data > max):
-        return False
-    
-    isLeft, isRight = isBST2(root.left ,min ,root.data-1), isBST2(root.right ,root.data ,max)
-    if not isLeft or not isRight:
-        return False
-    
-    return True
-    
+    def count(self,data):
+        return 0
 
-
+    def printTreeHelper(self,root):
+        if not root:
+            return None
         
+        print(root.data,end=":")
+        if root.left:
+            print("L",root.left.data,end=",")
+        if root.right:
+            print("R",root.right.data,end="")
+        print()
+        self.printTreeHelper(root.left)
+        self.printTreeHelper(root.right)
 
-root = levelWiseTreeInput()
-print(isBST2(root))
+    def printTree(self):
+        self.printTreeHelper(self.root)
 
 
+b = BST()
+b.insert(4)
+b.insert(2)
+b.insert(6)
+b.insert(5)
+b.insert(7)
+b.printTree()
