4386071a61128de5c7774e0762bd7267dd9cad3f
Naveen-ms <nagrawal.na1041@gmail.com>
2021-08-05 18:05:25 +0530

    Completed Heaps_1 or Priority_Queue_1

--- Coding Ninjas/Priority Queue/PQ.py
+++ Coding Ninjas/Priority Queue/PQ.py
@@ -9,70 +9,69 @@ class PriorityQueue:
     def __init__(self) -> None:
         self.pq = []
 
+    def isEmpty(self):
+        return self.getSize() == 0
+    
     def getSize(self):
         return len(self.pq)
 
-    def isEmpty(self):
-        return self.getSize() == 0
-
-    def getMin(self):
+    def getMax(self):
         if self.isEmpty():
             return None
         return self.pq[0].value
+      
 
     def __percolateUp(self):
-        childIndex = len(self.pq)-1
+        childIndex = self.getSize()-1
         while childIndex > 0:
             parentIndex = (childIndex-1)//2
-            if self.pq[childIndex].priority < self.pq[parentIndex].priority:
+
+            if self.pq[childIndex].priority > self.pq[parentIndex].priority:
                 self.pq[childIndex], self.pq[parentIndex] = self.pq[parentIndex], self.pq[childIndex]
                 childIndex = parentIndex
             else:
                 break
 
-    def insert(self,value,priority):
-
-        node = PriorityQueueNode(value,priority)
-        self.pq.append(node)
+    def insert(self,ele,priority):
+        Node = PriorityQueueNode(ele,priority)
+        self.pq.append(Node)
         self.__percolateUp()
 
 
     def __percolateDown(self):
-        
         parentIndex = 0
-        LeftChildIndex = 2*parentIndex+1
-        RightChildIndex = 2*parentIndex+2
+        leftChild = (2*parentIndex)+1
+        rightChild = (2*parentIndex)+2
 
-        while LeftChildIndex < self.getSize():
-            minIndex = parentIndex
+        while leftChild < self.getSize():
+            maxIndex = parentIndex
 
-            if self.pq[minIndex].priority > self.pq[LeftChildIndex].priority:
-                minIndex = LeftChildIndex
-            
-            if RightChildIndex < self.getSize() and self.pq[minIndex].priority > self.pq[RightChildIndex].priority:
-                minIndex = RightChildIndex
-            
-            if minIndex == parentIndex:
+            if self.pq[maxIndex].priority < self.pq[leftChild].priority:
+                maxIndex = leftChild
+            if rightChild < self.getSize() and self.pq[maxIndex].priority < self.pq[rightChild].priority:
+                maxIndex = rightChild
+            if maxIndex == parentIndex:
                 break
 
-            self.pq[parentIndex],self.pq[minIndex] = self.pq[minIndex], self.pq[parentIndex]
-            parentIndex = minIndex
-            LeftChildIndex = 2*parentIndex+1
-            RightChildIndex = 2*parentIndex+2
-                  
-    def removeMin(self):
+            self.pq[maxIndex], self.pq[parentIndex] = self.pq[parentIndex], self.pq[maxIndex]
+            parentIndex = maxIndex
+            leftChild = (2*parentIndex)+1
+            rightChild = (2*parentIndex)+2
+        
+    def removeMax(self):
         if self.isEmpty():
             return None
-        self.pq[0], self.pq[-1] = self.pq[-1], self.pq[0]
-        removedData = self.pq.pop().value
+        self.pq[-1], self.pq[0] = self.pq[0], self.pq[-1]
+        data = self.pq.pop().value
         self.__percolateDown()
-        return removedData
+        return data
 
 
 pq = PriorityQueue()
-pq.insert("A",10)
-pq.insert("C",5)
-pq.insert("B",90)
-pq.insert("D",4)
-for i in range(4):
-    print(pq.removeMin())
+pq.insert(5,5)
+pq.insert(7,7)
+pq.insert(3,3)
+pq.insert(9,9)
+pq.insert(1,1)
+for _ in range(5):
+    print(pq.removeMax())
