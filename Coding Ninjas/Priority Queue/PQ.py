d1f3ce7cc37361f5c6634dc12ca024adffe3962c
Naveen-ms <nagrawal.na1041@gmail.com>
2021-08-17 09:27:04 +0530

    Completed Priority Queue

--- Coding Ninjas/Priority Queue/PQ.py
+++ Coding Ninjas/Priority Queue/PQ.py
@@ -1,77 +1,69 @@
-# Complete Binary Tree (CBT)
-
-class PriorityQueueNode:
-    def __init__(self,value,priority) -> None:
-        self.value = value
-        self.priority = priority
-
-class PriorityQueue:
-    def __init__(self) -> None:
-        self.pq = []
-
-    def isEmpty(self):
-        return self.getSize() == 0
-    
-    def getSize(self):
-        return len(self.pq)
-
-    def getMax(self):
-        if self.isEmpty():
-            return None
-        return self.pq[0].value
-      
-
-    def __percolateUp(self):
-        childIndex = self.getSize()-1
-        while childIndex > 0:
-            parentIndex = (childIndex-1)//2
-
-            if self.pq[childIndex].priority > self.pq[parentIndex].priority:
-                self.pq[childIndex], self.pq[parentIndex] = self.pq[parentIndex], self.pq[childIndex]
-                childIndex = parentIndex
-            else:
-                break
-
-    def insert(self,ele,priority):
-        Node = PriorityQueueNode(ele,priority)
-        self.pq.append(Node)
-        self.__percolateUp()
-
-
-    def __percolateDown(self):
-        parentIndex = 0
-        leftChild = (2*parentIndex)+1
-        rightChild = (2*parentIndex)+2
-
-        while leftChild < self.getSize():
-            maxIndex = parentIndex
-
-            if self.pq[maxIndex].priority < self.pq[leftChild].priority:
-                maxIndex = leftChild
-            if rightChild < self.getSize() and self.pq[maxIndex].priority < self.pq[rightChild].priority:
-                maxIndex = rightChild
-            if maxIndex == parentIndex:
-                break
-
-            self.pq[maxIndex], self.pq[parentIndex] = self.pq[parentIndex], self.pq[maxIndex]
-            parentIndex = maxIndex
-            leftChild = (2*parentIndex)+1
-            rightChild = (2*parentIndex)+2
+import heapq as heap
+class LinkedListNode :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
         
-    def removeMax(self):
-        if self.isEmpty():
+class Queue :
+    def __init__(self) :
+        self.head = None 
+        self.tail = None
+        self.size = 0
+        
+    def enqueue(self, data) :
+        newNode = LinkedListNode(data)
+        if self.head is None :
+            self.head = self.tail = newNode
+        else :
+            self.tail.next = newNode
+            self.tail = newNode
+        self.size += 1
+        return
+        
+    def dequeue(self) :
+        if self.head is None :
             return None
-        self.pq[-1], self.pq[0] = self.pq[0], self.pq[-1]
-        data = self.pq.pop().value
-        self.__percolateDown()
+        data = self.head.data
+        self.head = self.head.next
+        self.size -= 1
         return data
+    
+    def getSize(self) :
+        return self.size
+    
+    def isEmpty(self) :
+        if self.head is None :
+            return True
+        return False
+    
+    def peek(self) :
+        if self.head is None :
+            return None
+        return self.head.data
 
+def buyTicket(arr, n, k) : 
+    q = Queue()
+    count = 1
+    heapMax = arr[:]
+    heap._heapify_max(heapMax)
+    for index in range(len(arr)):
+        q.enqueue(index)
 
-pq = PriorityQueue()
-pq.insert(5,5)
-pq.insert(7,7)
-pq.insert(3,3)
-pq.insert(9,9)
-pq.insert(1,1)
-for _ in range(5):
-    print(pq.removeMax())
+    while not q.isEmpty():
+        if heapMax[0] == arr[q.peek()] and q.peek() == k:
+            return count
+            
+        if heapMax[0] == arr[q.peek()]:
+            heap._heappop_max(heapMax)
+            q.dequeue()
+            count += 1
+        else:
+            val = q.dequeue()
+            q.enqueue(val)
+    
+    return None
+
+# li = [2,3,2,2,4]
+li = [3,9,4]
+print(buyTicket(li,len(li),2))
+
