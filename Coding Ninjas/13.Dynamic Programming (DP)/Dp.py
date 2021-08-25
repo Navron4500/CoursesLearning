7900b7886b63779e562ae4839f249c4c7cb87b5a
Naveen-ms <nagrawal.na1041@gmail.com>
2021-08-25 09:21:09 +0530

    Halfway DP 2

--- Coding Ninjas/13.Dynamic Programming (DP)/Dp.py
+++ Coding Ninjas/13.Dynamic Programming (DP)/Dp.py
@@ -1,57 +1,39 @@
-def LisIndexMemo(li,start,end,dp):
+from sys import stdin
+
+def knapsack(weights, values, n, maxWeight):
+    if maxWeight <= 0:
+        return 0
     
-    if start == end:
-        dp[start] = 1
-        return 1,1
-
-    including_max = 1
-    for i in range(start+1,end):
-        if li[i] > li[start]:
-            if dp[i] == -1:
-                ans = LisIndexMemo(li,i,end,dp)
-                dp[i] = ans
-                further_including_max = ans[0]
-            else:
-                further_including_max = dp[i][0]
-            including_max = max(further_including_max + 1,including_max)
+    if n == 1:
+        return values[0]
     
-    if dp[start+1] == -1:
-        ans = LisIndexMemo(li,start+1,end,dp)
-        dp[start+1] = ans
-        excluding_max = ans[1]
-    else:
-        excluding_max = dp[start+1][1]
+    excluding_Ans = knapsack(weights[1:],values[1:],n-1,maxWeight) # EXCLUDING
 
-    overall_max = max(including_max,excluding_max)
+    Including_Ans = weights[0] +  knapsack(weights[1:],values[1:],n-1,maxWeight - weights[0])   # INCLUDING
 
-    return including_max, overall_max
-
-def LisIterative(li,n):
-    dp = [[0 for j in range(2)] for i in range(n+1)]
-    # dp.append([1,1])
-    # dp.append([0,0])
-
-    for i in range(n-1,-1,-1):
-
-        including_max = 1
-        for j in range(i+1,n):
-            if li[j] > li[i]:
-                including_max = max(including_max,1+dp[j][0])
-            
-        dp[i][0] = including_max
-        
-        excluding_max = dp[i+1][1]
-
-        overallMax = max(including_max,excluding_max)
-        dp[i][1] = overallMax
-    
-    return dp[0][1]
-    
+    return max(excluding_Ans,Including_Ans)
 
 
-# li = [6,3,1,2,0,7,9] # 4
-# li = [1,7,2,3,4,5,6,8,9] # 8
-# li = [5,4,11,1,16,8] # 3
-li = [1,2,2] #2
-dp = [-1 for i in range(len(li)+1)]
-print(LisIterative(li,len(li)))
+
+	
+
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+
+    if n == 0 :
+        return list(), list(), n, 0
+
+    weights = list(map(int, stdin.readline().rstrip().split(" ")))
+    values = list(map(int, stdin.readline().rstrip().split(" ")))
+    maxWeight = int(stdin.readline().rstrip())
+
+    return weights, values, n, maxWeight
+
+
+#main
+# weights, values, n, maxWeight = takeInput()
+
+# weights, values, n, maxWeight =[1,2,4,5],[5,4,8,6],4,5 # 13
+weights, values, n, maxWeight = [12,7,11,8,9],[24,13,23,15,16],5,26 # 51
+
+print(knapsack(weights, values, n, maxWeight))
