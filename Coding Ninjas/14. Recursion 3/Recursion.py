0323ed3f7d306eb1fe7b03cc35d0cde2e377cf4d
Naveen-ms <nagrawal.na1041@gmail.com>
2021-08-27 19:29:01 +0530

    Completed Recursion 3

--- Coding Ninjas/14. Recursion 3/Recursion.py
+++ Coding Ninjas/14. Recursion 3/Recursion.py
@@ -1,25 +1,59 @@
-def keypad(n,d, soFar=""):
-    if n == 0:
-        print(soFar)
+import sys
+sys.setrecursionlimit(10 ** 8)
+
+
+
+
+def printAllSubsetsRec(arr, n, sum, v = []) :
+ 
+    # If remaining sum is 0, then print all
+    # elements of current subset.
+    if (sum == 0) :
+        v.reverse()
+        print(*v)
         return
-    
-    values = d[n]
+     
+ 
+    # If no remaining elements,
+    if (n == 0):
+        return
+ 
+    # We consider two cases for every element.
+    # a) We do not include last element.
+    # b) We include last element in current subset.
+    printAllSubsetsRec(arr, n - 1, sum, v)
+    v1 = [] + v
+    v1.append(arr[n - 1])
+    printAllSubsetsRec(arr, n - 1, sum - arr[n - 1], v1)
 
-    keypad(n//10,d,values[0]+soFar)
 
 
+#taking input
+def takeInput() :
+    n = int(input().strip())
 
-d = {
-    2:["a","b","c"],
-    3:["d","e","f"],
-    4:["g","h","i"],
-    5:["j","k","l"],
-    6:["m","n","o"],
-    7:["p","q","r","s"],
-    8:["t","u","v"],
-    9:["w","x","y","z"]
-}
+    if n == 0 :
+        return list(), 0
 
-n = 23
-ans = keypad(n,d)
-print(*ans)
+    arr = [int(element) for element in list(input().strip().split(" "))]
+    return arr, n
+
+
+#printing the list of lists
+def printListOfList(liOfLi) :
+    for li in liOfLi :
+        for elem in li :
+            print(elem, end = " ")
+        print()
+
+#main
+# arr, n = [5,12,3,17,1,18,15,3,7], 9
+arr,n = [4,3,1],3
+
+if n != 0 :
+    k = 5
+    liOfLi = printAllSubsetsRec(arr, n, k)
+    # print(liOfLi)
+
+    # printListOfList(liOfLi)
+
